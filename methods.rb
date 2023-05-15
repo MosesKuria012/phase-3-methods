b3e02865d03a4214d1fe5594500860889c557eee
Moses <t.content012@gmail.com>
2023-05-15 19:20:55 +0300

    first commit

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
-# Your code here!
+def greet_programmer ()
+  puts ("Hello, programmer!")
+end
+
+def greet(name = 'Naureen') 
+  puts ("Hello, #{name}!")
+end
+
+def greet_with_default (name = 'Naureen')
+  puts ("Hello, #{name}!")
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+  return "Hello, #{name}!"
+end
+
+def add (num1, num2)
+    return (num1 + num2)
+end 
+sum1 = add(2,7)
+
+
+def halve (num)
+    return nil unless num.is_a?(Integer)
+  num / 2
+end
+
+
+
